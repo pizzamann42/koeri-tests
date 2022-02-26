37ebb4c4e223d7e3263714299cb9cad6fe7828eb
koerihub <koeri@cock.li>
2022-02-26 13:08:37 +0100

    Test list side effects

--- src/test/java/edu/kit/informatik/NetworkListTest.java
+++ src/test/java/edu/kit/informatik/NetworkListTest.java
@@ -1,10 +1,13 @@
 package edu.kit.informatik;
 
 import edu.kit.informatik.util.KoeriTestUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -19,6 +22,28 @@ public class NetworkListTest {
     @MethodSource("listProvider")
     void testList(List<IP> list, Stream<Network> networks) {
         networks.forEach(network -> assertEquals(list, network.list()));
+    }
+
+    @Test
+    void testListSideEffects() {
+        Network original = network(0, "a_random");
+        Network network = network(0, "a_random");
+        List<IP> list = network.list();
+        tryModify(() -> list.add(ip("45.12.93.120")));
+        assertEquals(original, network);
+        tryModify(() -> Collections.shuffle(list));
+        assertEquals(original, network);
+        tryModify(list::clear);
+        assertEquals(original, network);
+    }
+
+    void tryModify(Executable exe) {
+        try {
+            exe.execute();
+        } catch (UnsupportedOperationException ignored) {
+        } catch (Throwable e) {
+            throw new AssertionError(e);
+        }
     }
 
     static Stream<Arguments> listProvider() {
