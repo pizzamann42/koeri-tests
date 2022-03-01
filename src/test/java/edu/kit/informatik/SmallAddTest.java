05994df1768ad8fcaa4fbc9d24b452dd5f7f1c8d
pizzamann42 <100698226+pizzamann42@users.noreply.github.com>
2022-03-01 14:53:33 +0100

    Add files via upload

---
+++ src/test/java/edu/kit/informatik/SmallAddTest.java
@@ -0,0 +1,27 @@
+package edu.kit.informatik;
+
+import edu.kit.informatik.exceptions.ParseException;
+import edu.kit.informatik.network.IP;
+import edu.kit.informatik.network.Network;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static edu.kit.informatik.util.KoeriTestUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+class SmallAddTest {
+
+    /*
+    This test adds 2 independent trees to a network with 1 existing trees
+    The 2 trees have to be added to the same tree.
+     */
+    @Test
+    public void testAdd() throws ParseException {
+        Network net1 = new Network("(1.1.1.1 2.2.2.2 3.3.3.3)");
+        Network net2 = new Network("(3.3.3.3 4.4.4.4 5.5.5.5)");
+        Network net3 = new Network("(2.2.2.2 6.6.6.6 7.7.7.7)");
+        net2.add(net3);
+        net1.add(net2);
+        assertEquals(net1.toString(new IP("1.1.1.1")), "(1.1.1.1 (2.2.2.2 6.6.6.6 7.7.7.7) (3.3.3.3 4.4.4.4 5.5.5.5))");
+    }
+}
